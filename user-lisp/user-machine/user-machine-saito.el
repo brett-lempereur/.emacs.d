84e0d4d00eb1fa701a80af0a9fabac5774d68d74
Brett Lempereur <naufauna@gmail.com>
2024-06-16 21:45:49 +0100

    Rename development laptop

--- user-lisp/user-machine/user-machine-saito.el
+++ user-lisp/user-machine/user-machine-saito.el
@@ -1,4 +1,4 @@
-;;; user-machine-nagata -- Personal development workstation customisations.
+;;; user-machine-saito -- Personal development workstation customisations.
 
 ;;; Commentary:
 
@@ -11,11 +11,11 @@
 ;; Appearance customisations
 (customize-set-variable 'user-setting-font "Source Code Pro-13")
 (customize-set-variable 'user-setting-menu-bar-mode t)
-(customize-set-variable 'user-setting-theme-package 'kanagawa-theme)
-(customize-set-variable 'user-setting-theme 'kanagawa)
+(customize-set-variable 'user-setting-theme-package 'acme-theme)
+(customize-set-variable 'user-setting-theme 'acme)
 
 ;; Racket programming language customisations
 (customize-set-variable 'user-setting-racket-program "/opt/racket/bin/racket")
 
-(provide 'user-machine-nagata)
-;;; user-machine-nagata.el ends here
+(provide 'user-machine-saito)
+;;; user-machine-saito.el ends here
