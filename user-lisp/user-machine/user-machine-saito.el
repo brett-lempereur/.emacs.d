1b3298393820a29a6aabbaae21cef80eefa2c528
Brett Lempereur <naufauna@gmail.com>
2024-07-22 23:11:11 +0100

    Change to a light theme

--- user-lisp/user-machine/user-machine-saito.el
+++ user-lisp/user-machine/user-machine-saito.el
@@ -11,8 +11,8 @@
 ;; Appearance customisations
 (customize-set-variable 'user-setting-font "Source Code Pro-14")
 (customize-set-variable 'user-setting-menu-bar-mode t)
-(customize-set-variable 'user-setting-theme-package 'nano-theme)
-(customize-set-variable 'user-setting-theme 'nano-dark)
+(customize-set-variable 'user-setting-theme-package 'ef-themes)
+(customize-set-variable 'user-setting-theme 'ef-kassio)
 
 ;; Racket programming language customisations
 (customize-set-variable 'user-setting-racket-program "/opt/racket/bin/racket")
